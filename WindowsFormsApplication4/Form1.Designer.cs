db887e26967921bf5bded50ac66acafe94517244
PC <PC@DESKTOP-OGTDQDE>
2018-07-10 23:43:18 -0400

    Add project files.

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNIngresar = new System.Windows.Forms.Button();
+            this.TXTID = new System.Windows.Forms.TextBox();
+            this.TXTOS = new System.Windows.Forms.TextBox();
+            this.CHKAOT = new System.Windows.Forms.CheckBox();
+            this.TXTURL = new System.Windows.Forms.TextBox();
+            this.CHKExtras = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BTNIngresar
+            // 
+            this.BTNIngresar.Location = new System.Drawing.Point(12, 63);
+            this.BTNIngresar.Name = "BTNIngresar";
+            this.BTNIngresar.Size = new System.Drawing.Size(100, 23);
+            this.BTNIngresar.TabIndex = 0;
+            this.BTNIngresar.Text = "Ingresar";
+            this.BTNIngresar.UseVisualStyleBackColor = true;
+            this.BTNIngresar.Click += new System.EventHandler(this.BTNIngresar_Click);
+            // 
+            // TXTID
+            // 
+            this.TXTID.Location = new System.Drawing.Point(12, 11);
+            this.TXTID.Name = "TXTID";
+            this.TXTID.Size = new System.Drawing.Size(100, 20);
+            this.TXTID.TabIndex = 1;
+            this.TXTID.Text = "ID";
+            this.TXTID.Enter += new System.EventHandler(this.TXTID_Enter);
+            this.TXTID.Leave += new System.EventHandler(this.TXTID_Leave);
+            // 
+            // TXTOS
+            // 
+            this.TXTOS.Location = new System.Drawing.Point(12, 37);
+            this.TXTOS.Name = "TXTOS";
+            this.TXTOS.Size = new System.Drawing.Size(100, 20);
+            this.TXTOS.TabIndex = 2;
+            this.TXTOS.Text = "OS";
+            this.TXTOS.Enter += new System.EventHandler(this.TXTOS_Enter);
+            this.TXTOS.Leave += new System.EventHandler(this.TXTOS_Leave);
+            // 
+            // CHKAOT
+            // 
+            this.CHKAOT.AutoSize = true;
+            this.CHKAOT.Location = new System.Drawing.Point(12, 156);
+            this.CHKAOT.Name = "CHKAOT";
+            this.CHKAOT.Size = new System.Drawing.Size(97, 17);
+            this.CHKAOT.TabIndex = 3;
+            this.CHKAOT.Text = "Siempre Visible";
+            this.CHKAOT.UseVisualStyleBackColor = true;
+            this.CHKAOT.CheckedChanged += new System.EventHandler(this.CHKAOT_CheckedChanged);
+            // 
+            // TXTURL
+            // 
+            this.TXTURL.Location = new System.Drawing.Point(12, 130);
+            this.TXTURL.Name = "TXTURL";
+            this.TXTURL.Size = new System.Drawing.Size(100, 20);
+            this.TXTURL.TabIndex = 4;
+            this.TXTURL.Text = "http://localhost/post/simplepost.php";
+            this.TXTURL.Enter += new System.EventHandler(this.TXTURL_Enter);
+            this.TXTURL.Leave += new System.EventHandler(this.TXTURL_Leave);
+            // 
+            // CHKExtras
+            // 
+            this.CHKExtras.AutoSize = true;
+            this.CHKExtras.Location = new System.Drawing.Point(12, 99);
+            this.CHKExtras.Name = "CHKExtras";
+            this.CHKExtras.Size = new System.Drawing.Size(55, 17);
+            this.CHKExtras.TabIndex = 6;
+            this.CHKExtras.Text = "Extras";
+            this.CHKExtras.UseVisualStyleBackColor = true;
+            this.CHKExtras.CheckedChanged += new System.EventHandler(this.CHKExtras_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(123, 126);
+            this.Controls.Add(this.CHKExtras);
+            this.Controls.Add(this.TXTURL);
+            this.Controls.Add(this.CHKAOT);
+            this.Controls.Add(this.TXTOS);
+            this.Controls.Add(this.TXTID);
+            this.Controls.Add(this.BTNIngresar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNIngresar;
+        private System.Windows.Forms.TextBox TXTID;
+        private System.Windows.Forms.TextBox TXTOS;
+        private System.Windows.Forms.CheckBox CHKAOT;
+        private System.Windows.Forms.TextBox TXTURL;
+        private System.Windows.Forms.CheckBox CHKExtras;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNIngresar_Click(object sender, EventArgs e)
+        {
+            if ((TXTID.Text == "" && TXTOS.Text == "")||(TXTID.Text=="ID"&&TXTOS.Text=="OS"))
+            {
+                MessageBox.Show("Debe ingresar un ID y una OS", "Faltan datos", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (var wb = new WebClient())
+                {
+                    var data = new NameValueCollection();
+                    data["ID"] = TXTID.Text;
+                    data["OS"] = TXTOS.Text;
+                    var url = TXTURL.Text;
+                    var response = wb.UploadValues(url, "POST", data);
+                    string responseInString = Encoding.UTF8.GetString(response);
+                    MessageBox.Show(Encoding.UTF8.GetString(response), "Datos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                TXTID.Text = "ID";TXTOS.Text = "OS";
+            }
+        }
+
+        private void TXTID_Enter(object sender, EventArgs e)
+        {
+            if (TXTID.Text == "ID")
+            {
+                TXTID.Text = "";
+            }
+        }
+
+        private void TXTOS_Enter(object sender, EventArgs e)
+        {
+            if (TXTOS.Text == "OS")
+            {
+                TXTOS.Text = "";
+            }
+        }
+
+        private void TXTID_Leave(object sender, EventArgs e)
+        {
+            if (TXTID.Text == "") {
+                TXTID.Text = "ID";
+            }
+        }
+
+        private void TXTOS_Leave(object sender, EventArgs e)
+        {
+            if (TXTOS.Text == "")
+            {
+                TXTOS.Text = "OS";
+            }
+        }
+
+        private void CHKExtras_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CHKExtras.Checked)
+            {
+                this.Height = 220;
+            }else
+            {
+                this.Height = 165;
+            }
+            
+        }
+
+        private void CHKAOT_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CHKAOT.Checked)
+            {
+                this.TopMost=true;
+            }else
+            {
+                this.TopMost = false;
+            }
+
+        }
+
+        private void TXTURL_Enter(object sender, EventArgs e)
+        {
+            TXTURL.Text = "";
+        }
+
+        private void TXTURL_Leave(object sender, EventArgs e)
+        {
+            if (TXTURL.Text == "")
+            {
+                TXTURL.Text = "http://localhost/post/simplepost.php";
+            }
+        }
+    }
+}
